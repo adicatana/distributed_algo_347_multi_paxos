1eae322c530028607f71c3780b85f18b24d53263
Adi Catana <adrian.catana15@imperial.ac.uk>
2018-02-18 17:19:49 +0000

    Implementation of propose/using queue now for requests

--- lib/replica.ex
+++ lib/replica.ex
@@ -2,27 +2,64 @@
 
 defmodule Replica do
 
+  def start config, database, monitor do
+    receive do
+      {:bind, leaders} ->
+        next database, 1, 1, :queue.new, MapSet.new, MapSet.new, leaders, config.window_size
+    end
+  end
+
+  def next state, slot_in, slot_out, requests, proposals, decisions, leaders, window do
+    receive do
+      {:client_request, c} ->
+        requests = :queue.in(c, requests)
+
+      {:decision, s, c} -> 
+      # decision made by the Synod protocol, command c for slot s
+        decisions = MapSet.put(decisions, {s, c})
+
+        # For all the decisions that are ready for execution
+        for {^slot_out, command} <- decisions do
+          perform(command)
+        end
+    end
+    propose state, slot_in, slot_out, requests, proposals, decisions, leaders, window
+  end
+
   def isreconfig op do
     match?({_, _, {:reconfig, _}} ,op)
   end
 
   def propose state, slot_in, slot_out, requests, proposals, decisions, leaders, window do
-    for c <- decisions, slot_in < slot_out + window do
-      slot_id = slot_in - window
-      for {^slot_id, {_, _, op}} <- decisions, isreconfig(op) do
-        leaders = op.leaders
-      end
-
+    if slot_in < slot_out + window and !:queue.is_empty(requests) do
       if !Enum.find(decisions, fn d -> match?({^slot_in, _}, d) end) do
-        requests = MapSet.difference(requests, MapSet.put(Mapset.new, c))
-        proposals = MapSet.union(proposals, {slot_in, c})
-        for l <- leaders, do:
-            send l, {:propose, slot_in, c}
+        {{:value, c}, requests} = :queue.out(requests)
+        proposals = Mapset.unin(proposals, {slot_in, c})
+        for l <- leaders do
+          send l, {:propose, slot_in, c}
+        end
       end
       slot_in = slot_in + 1
+      propose state, slot_in, slot_out, requests, proposals, decisions, leaders, window
     end
 
-    next state, slot_in, slot_out, requests, proposals, decisions, leaders, window
+
+#    for c <- decisions, slot_in < slot_out + window do
+#      slot_id = slot_in - window
+#      for {^slot_id, {_, _, op}} <- decisions, isreconfig(op) do
+#        leaders = op.leaders
+#      end
+
+#      if !Enum.find(decisions, fn d -> match?({^slot_in, _}, d) end) do
+#        requests = MapSet.difference(requests, MapSet.put(Mapset.new, c))
+#        proposals = MapSet.union(proposals, {slot_in, c})
+#        for l <- leaders, do:
+#            send l, {:propose, slot_in, c}
+#      end
+#      slot_in = slot_in + 1
+#    end
+
+#    next state, slot_in, slot_out, requests, proposals, decisions, leaders, window
   end
 
   def perform {client, cid, op}, state, slot_in, slot_out, requests, proposals, decisions, leaders, window do
@@ -44,26 +81,4 @@ defmodule Replica do
     next state, slot_in, slot_out, requests, proposals, decisions, leaders, window
   end
 
-  def start config, database, monitor do
-    receive do
-      {:bind, leaders} ->
-        next database, 1, 1, MapSet.new, MapSet.new, MapSet.new, leaders, config.window_size
-    end
-  end
-
-  def next state, slot_in, slot_out, requests, proposals, decisions, leaders, window do
-    receive do
-      {:client_request, c} ->
-        requests = MapSet.put(requests, c)
-      {:decision, s, c} -> # decision made by the Synod protocol, command c for slot s
-        decisions = MapSet.put(decisions, {s, c})
-
-        # For all the decisions that are ready for execution
-        for {^slot_out, command} <- decisions do
-
-          perform(command)
-        end
-    end
-    propose state, slot_in, slot_out, requests, proposals, decisions, leaders, window
-  end
 end
