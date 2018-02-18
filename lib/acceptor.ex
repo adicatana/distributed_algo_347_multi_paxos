7791f2afd018b7f1e95e44924e052b2b41577982
Adi Catana <adrian.catana15@imperial.ac.uk>
2018-02-18 14:23:03 +0000

    Reviewing + refactoring + understading codebase

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -6,20 +6,36 @@ defmodule Acceptor do
     next (-1, -1), MapSet.new
   end
 
-  def next b, accepted do
+  # Acceptor runs in an infinite loop, receiving two 
+  # kinds of request messages from leaders
+  def next ballot_num, accepted do
     receive do
-      {:p1a, leader, ballot_num} ->
-        if ballot_num > b do
-          b = ballot_num
+      # ⟨p1a,l,b⟩: receive a phase 1a request message 
+      # from a leader with identifier l, for a ballot number b, 
+      # an acceptor makes the following transition.  
+      # Acceptor adopts b iff it exceeds its current
+      # ballot number. Then it returns to l a phase1b response 
+      # message containing its current ballot number and all 
+      # pvalues accepted thus far by the acceptor.
+      {:p1a, l, b} ->
+        if b > ballot_num do
+          ballot_num = b
         end
-        send leader, {:p1b, self(), b, accepted}
-      {:p2a, leader, {ballot_num, s, c}} ->
-        if b == ballot_num do
+        send l, {:p1b, self(), ballot_num, accepted}
+
+      # ⟨p2a,l,⟨b,s,c⟩⟩: receive a phase 2a request 
+      # message from leader l with pvalue ⟨b,s,c⟩, 
+      # If the current ballot number equals b, then 
+      # the acceptor accepts ⟨b,s,c⟩. Acceptor 
+      # returns to l a phase 2b response message 
+      # containing its current ballot number.        
+      {:p2a, l, {b, s, c}} ->
+        if ballot_num == b do
           accepted = MapSet.put(accepted, {b, s, c})
         else
-          send leader, {:p2b, self(), b}
+          send l, {:p2b, self(), ballot_num}
         end
     end
-    next b, accepted
+    next ballot_num, accepted
   end
 end
